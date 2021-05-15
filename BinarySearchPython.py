cd0872c7f904d960f1f921c73402033bfd400647
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-05-15 10:45:50 +0530

    Create BinarySearchPython.py
    first commit

---
+++ BinarySearchPython.py
@@ -0,0 +1,54 @@
+
+# Array (List)
+nums = [1,3,5,6]
+
+# The Function of Binary Search with Itterative Approach
+def binary_search(arr, key):
+    '''
+    arr --> is the list or array
+    key --> Element to Search for
+    '''
+    l = 0
+    h = len(arr)
+    mid = (l+h)//2
+    
+    
+    while(l<h):
+        if key>arr[mid]:
+            l = mid+1
+            mid = (l+h)//2
+        if key<arr[mid]:
+            h = mid-1
+            mid = (l+h)//2
+        else:
+            return mid
+
+
+
+# Another Simple Array
+A = [1,2,3,5,8]
+
+def recBin_search(l, h, key):
+    '''
+    l --> low index
+    h --> high index
+    key --> Element to search for
+    '''
+    if l==h:
+        if A[l]==key:
+            return l
+        else:
+            return 0
+    else:
+        mid=(l+h)//2
+        if key==A[mid]:
+            return mid
+        if key<A[mid]:
+            return recBin_search(l, mid-1, key)
+        else:
+            return recBin_search(mid+1, h, key)
+
+#Give the Key Element 
+print(recBin_search(0, len(A), key=3))
+
+print(binary_search(nums, key=2))
